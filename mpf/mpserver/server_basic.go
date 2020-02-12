b73fcc77a9c7c883d362e95c7938b92cf3fd17d6
jiangwei <jiangwei07061625@163.com>
2020-02-13 02:10:16 +0800

    study iris

--- mpf/mpserver/server_basic.go
+++ mpf/mpserver/server_basic.go
@@ -1,9 +1,13 @@
 package mpserver
 
 import (
-    "net"
+    "context"
+    "fmt"
+    "io/ioutil"
     "net/http"
     "os"
+    "os/signal"
+    "strconv"
     "sync"
     "syscall"
     "time"
@@ -12,94 +16,171 @@ import (
     "github.com/a07061625/gompf/mpf/mplog"
 )
 
-type IServerBasic interface {
-    ForkProcess(serverMode string) int
-    Start()
-    Shutdown(timeout time.Duration)
-}
-
 type serverBasic struct {
-    http.Server
-    Listener    *serverListener
-    connManager *connManager
+    server    http.Server
+    serverTag string                   // 服务标识
+    appFlag   bool                     // app初始化标识 true:已初始化 false:未初始化
+    timeout   map[string]time.Duration // 超时时间列表
+    pidFile   string                   // 进程ID文件
+    pid       int                      // 进程ID
 }
 
-// 创建服务进程 daemon:守护进程 child:子进程
-func (s *serverBasic) ForkProcess(serverMode string) int {
-    os.Setenv(mpf.GoEnvServerMode, serverMode)
-
-    l := NewListener(s.Server.Addr)
-    files := make([]uintptr, 0)
-    if serverMode == mpf.EnvServerModeChild {
-        lFd, err := l.File()
-        if err != nil {
-            mplog.LogFatal(serverMode + " server fork error: " + err.Error())
-        }
-        files = []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd(), lFd}
-    } else {
-        files = []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()}
+func (s *serverBasic) getPid() int {
+    pid := 0
+    if f, err := os.Open(s.pidFile); err == nil {
+        pidStr, _ := ioutil.ReadAll(f)
+        pid, _ = strconv.Atoi(string(pidStr))
+        defer f.Close()
     }
-
-    attr := &syscall.ProcAttr{
-        Env:   os.Environ(),
-        Files: files,
-    }
-    pid, err := syscall.ForkExec(os.Args[0], os.Args, attr)
-    if err != nil {
-        mplog.LogFatal(serverMode + " server fork error: " + err.Error())
-    }
-    s.Listener = l
 
     return pid
 }
 
-func (s *serverBasic) Start() {
-    err := s.Server.Serve(s.Listener)
+func (s *serverBasic) savePid(pid int) {
+    f, err := os.OpenFile(s.pidFile, os.O_CREATE|os.O_WRONLY, 0664)
     if err != nil {
-        mplog.LogFatal(os.Getenv(mpf.GoEnvServerMode) + " server start error: " + err.Error())
+        mplog.LogInfo("write pid file error: " + err.Error())
+        return
+    }
+    defer f.Close()
+    f.WriteString(strconv.Itoa(pid))
+}
+
+// 发一个信号为0到指定进程ID,如果没有错误发生,表示进程存活
+func (s *serverBasic) checkRunning() bool {
+    if s.pid <= 0 {
+        return false
+    }
+
+    err := syscall.Kill(s.pid, 0)
+    return err == nil
+}
+
+func (s *serverBasic) GetPidFile() string {
+    return s.pidFile
+}
+
+func (s *serverBasic) SetApp(app http.Handler) {
+    if s.appFlag {
+        return
+    }
+
+    s.server.Handler = app
+    s.server.Addr = mpf.EnvServerDomain()
+}
+
+// 捕获系统信号
+func (s *serverBasic) listenNotify() {
+    signals := make(chan os.Signal)
+    signal.Notify(signals, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR2)
+
+    select {
+    case sig := <-signals:
+        mplog.LogInfo("server " + s.serverTag + " shutdown by signal " + sig.String())
+        ctx, _ := context.WithTimeout(context.Background(), insBasic.timeout["shutdown"])
+        s.server.Shutdown(ctx)
     }
 }
 
-// 关闭服务
-func (s *serverBasic) Shutdown(timeout time.Duration) {
-    s.SetKeepAlivesEnabled(false)
-    s.connManager.Close(timeout)
-    logMsg := os.Getenv(mpf.GoEnvServerMode) + " server shutdown "
-    err := s.Listener.Close()
-    if err != nil {
-        mplog.LogError(logMsg + "error: " + err.Error())
+// 启动服务
+//   startType: int 1:启动 2:重启
+func (s *serverBasic) Start(startType int) {
+    logMsg := "server " + s.serverTag
+    if s.checkRunning() {
+        fmt.Println(logMsg + " is running")
+        return
+    }
+    if s.server.Handler == nil {
+        fmt.Println(logMsg + " not bind app")
+        return
+    }
+
+    if startType == 1 {
+        logMsg += " start "
     } else {
-        mplog.LogInfo(logMsg + "success")
+        logMsg += " restart "
+    }
+    err := s.server.ListenAndServe()
+    if err != nil {
+        fmt.Println(logMsg + "error: " + err.Error())
+        return
+    }
+
+    fmt.Println(logMsg + "success")
+    s.savePid(os.Getpid())
+    s.listenNotify()
+}
+
+func (s *serverBasic) Stop() {
+    logMsg := "server " + s.serverTag
+    if !s.checkRunning() {
+        fmt.Println(logMsg + " already stoped")
+        return
+    }
+
+    err := syscall.Kill(s.pid, syscall.SIGTERM)
+    if err == nil {
+        fmt.Println(logMsg + " stop success")
+    } else {
+        fmt.Println(logMsg + " stop error: " + err.Error())
+    }
+}
+
+func (s *serverBasic) Restart() {
+    stopStatus := make(chan bool, 1)
+    go func() {
+        for {
+            if !s.checkRunning() {
+                stopStatus <- true
+                break
+            }
+            time.Sleep(500 * time.Millisecond)
+        }
+    }()
+
+    select {
+    case status := <-stopStatus:
+        if status {
+            s.Start(2)
+        }
+    case <-time.After(10 * time.Second):
+        fmt.Println("server " + s.serverTag + " restart timeout")
     }
 }
 
 var (
-    once sync.Once
-    ins  *serverBasic
+    onceBasic sync.Once
+    insBasic  *serverBasic
 )
 
-func New(addr string, app http.Handler) *serverBasic {
-    once.Do(func() {
-        ins = &serverBasic{
-            Server: http.Server{
-                Addr:    addr,
-                Handler: app,
-            },
-            connManager: newConnManager(),
+func init() {
+    insBasic = &serverBasic{}
+    insBasic.appFlag = false
+}
+
+func NewBasic() *serverBasic {
+    onceBasic.Do(func() {
+        insBasic.serverTag = mpf.EnvProjectKey() + strconv.Itoa(mpf.EnvServerPort())
+        insBasic.pidFile = mpf.EnvDirRoot() + "/pid/" + insBasic.serverTag + ".pid"
+        insBasic.pid = insBasic.getPid()
+
+        conf := mpf.NewConfig().GetConfig("server")
+        confPrefix := mpf.EnvType() + "." + mpf.EnvProjectKeyModule() + ".timeout."
+        insBasic.timeout = make(map[string]time.Duration)
+        insBasic.timeout["read"] = time.Duration(conf.GetInt(confPrefix+"read")) * time.Second
+        insBasic.timeout["readheader"] = time.Duration(conf.GetInt(confPrefix+"readheader")) * time.Second
+        insBasic.timeout["write"] = time.Duration(conf.GetInt(confPrefix+"write")) * time.Second
+        insBasic.timeout["idle"] = time.Duration(conf.GetInt(confPrefix+"idle")) * time.Second
+        insBasic.timeout["shutdown"] = time.Duration(conf.GetInt(confPrefix+"shutdown")) * time.Second
+
+        insBasic.server = http.Server{
+            ReadTimeout:       insBasic.timeout["read"],
+            ReadHeaderTimeout: insBasic.timeout["readheader"],
+            WriteTimeout:      insBasic.timeout["write"],
+            IdleTimeout:       insBasic.timeout["idle"],
         }
-        ins.ConnState = func(conn net.Conn, state http.ConnState) {
-            switch state {
-            case http.StateNew:
-                ins.connManager.Add(1)
-            case http.StateActive:
-                ins.connManager.RemoveIdleConn(conn.LocalAddr().String())
-            case http.StateIdle:
-                ins.connManager.AddIdleConn(conn.LocalAddr().String(), conn)
-            case http.StateHijacked, http.StateClosed:
-                ins.connManager.Done()
-            }
-        }
+        insBasic.server.SetKeepAlivesEnabled(false)
     })
 
-    return ins
+    return insBasic
 }
---
+++ mpf/mpserver/server_demo.go
@@ -0,0 +1,103 @@
+package mpserver
+
+import (
+    "net"
+    "net/http"
+    "os"
+    "sync"
+    "syscall"
+    "time"
+
+    "github.com/a07061625/gompf/mpf"
+    "github.com/a07061625/gompf/mpf/mplog"
+)
+
+type serverDemo struct {
+    http.Server
+    pid             int           // 进程ID
+    pidFile         string        // 进程ID文件
+    timeoutShutdown time.Duration // 进程关闭超时时间
+    serverTag       string        // 服务标识
+    Listener        *serverListener
+    connManager     *connManager
+}
+
+// 创建服务进程 daemon:守护进程 child:子进程
+func (s *serverDemo) ForkProcess(serverMode string) int {
+    os.Setenv(mpf.GoEnvServerMode, serverMode)
+
+    l := NewListener(s.Server.Addr)
+    files := make([]uintptr, 0)
+    if serverMode == mpf.EnvServerModeChild {
+        lFd, err := l.File()
+        if err != nil {
+            mplog.LogFatal(serverMode + " server fork error: " + err.Error())
+        }
+        files = []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd(), lFd}
+    } else {
+        files = []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()}
+    }
+
+    attr := &syscall.ProcAttr{
+        Env:   os.Environ(),
+        Files: files,
+    }
+    pid, err := syscall.ForkExec(os.Args[0], os.Args, attr)
+    if err != nil {
+        mplog.LogFatal(serverMode + " server fork error: " + err.Error())
+    }
+    s.Listener = l
+
+    return pid
+}
+
+func (s *serverDemo) Start() {
+    err := s.Server.Serve(s.Listener)
+    if err != nil {
+        mplog.LogFatal(os.Getenv(mpf.GoEnvServerMode) + " server start error: " + err.Error())
+    }
+}
+
+// 关闭服务
+func (s *serverDemo) Shutdown(timeout time.Duration) {
+    s.SetKeepAlivesEnabled(false)
+    s.connManager.Close(timeout)
+    logMsg := os.Getenv(mpf.GoEnvServerMode) + " server shutdown "
+    err := s.Listener.Close()
+    if err != nil {
+        mplog.LogError(logMsg + "error: " + err.Error())
+    } else {
+        mplog.LogInfo(logMsg + "success")
+    }
+}
+
+var (
+    onceDemo sync.Once
+    insDemo  *serverDemo
+)
+
+func NewDemo(addr string, app http.Handler) *serverDemo {
+    onceDemo.Do(func() {
+        insDemo = &serverDemo{
+            Server: http.Server{
+                Addr:    addr,
+                Handler: app,
+            },
+            connManager: newConnManager(),
+        }
+        insDemo.ConnState = func(conn net.Conn, state http.ConnState) {
+            switch state {
+            case http.StateNew:
+                insDemo.connManager.Add(1)
+            case http.StateActive:
+                insDemo.connManager.RemoveIdleConn(conn.LocalAddr().String())
+            case http.StateIdle:
+                insDemo.connManager.AddIdleConn(conn.LocalAddr().String(), conn)
+            case http.StateHijacked, http.StateClosed:
+                insDemo.connManager.Done()
+            }
+        }
+    })
+
+    return insDemo
+}
